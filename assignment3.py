3e3bc1c71056658ce39eeb66cdc8780033d2b5c3
Jarrett Strow <jarrett.c.strow-1@ou.edu>
2023-02-14 14:52:15 -0600

    Added the ability to receive user input for a list and output the correct sum and products of that list.

--- assignment3.py
+++ assignment3.py
@@ -1,15 +1,27 @@
 # Jarrett Strow
 # Assignment 3
 
-def arraysum(thelist):
+def arraysum(thelist): # calculates the total sum of all elements of an array
     sum = 0
     for element in thelist:
         sum = sum + element
     return sum
 
-def arrayprod(thelist):
+def arrayprod(thelist): # calculates the total product of all elements of an array
     product = 1
     for element in thelist:
         product = product * element
     return product
+
+# User inputted values
+size = int(input("Enter the size of the array: "))
+list = []
+
+for i in range(0, size):
+    num = int(input("Enter an integer for the %d element of the array: " % i))
+
+    list.append(num)
+
+print("The total sum of your inputted array is: " + str(arraysum(list)) + ", and the product of your entire array is: " + str(arrayprod(list)))
+
 
